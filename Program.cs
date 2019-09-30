70f7d485609d8af149738007522d1f1c3e9cf95f
Hien Thai Le <hientle@kms.com.vn>
2019-09-30 11:25:21 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Diagnostics;
+using System.IO;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace _4TellPipelineAutomationTestConsoleApp
+{
+  
+    class Program
+    {
+        
+       
+        static void Main(string[] args)
+        {
+            string driverOptions;
+            IWebDriver driver;
+            if (args.Length < 1)
+            {
+                driverOptions = "headless";
+
+
+            }
+            else
+            {
+                string argument = args[0].ToString().ToLower();
+                driverOptions = argument;
+            }
+            //driverOptions = "noheadless"; D:\a\1\s
+            string startupPath = "D:\\Jenkins\\workspace\\Sample@2\\drivers";
+            //Console.WriteLine(startupPath);
+            //driver = new ChromeDriver(startupPath);
+            ChromeOptions options = new ChromeOptions();
+            if (driverOptions == "headless")
+            {
+
+                options.AddArgument("--headless");
+            }
+            driver = new ChromeDriver(startupPath, options);
+            driver.Navigate().GoToUrl("https://www.google.com");
+            string pageTitle = driver.Title;
+            Assert.IsTrue(pageTitle.ToLower().Contains("google"));
+            driver.Quit();
+            Console.WriteLine("TEST END>>>>>>>>>>>>>>>>>>>>");
+        }
+        //public void NavigateToGoogle()
+        //{
+        //    createChromeDriver();
+        //    string startupPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName, "\\drivers\\chromedriver.exe");
+        //    Debug.WriteLine(startupPath);
+        //    driver.Navigate().GoToUrl("https://www.google.com");
+        //    string pageTitle = driver.Title;
+        //    Assert.IsTrue(pageTitle.ToLower().Contains("google"));
+        //}
+        //public void createChromeDriver(string driverOptions)
+        //{
+        //    //string startupPath = Path.Combine(Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName, "\\drivers\\chromedriver.exe");
+        //    string startupPath = Directory.GetParent(System.IO.Directory.GetCurrentDirectory()).Parent.FullName + "\\drivers";
+        //    //Console.WriteLine(startupPath);
+        //    //driver = new ChromeDriver(startupPath);
+        //    ChromeOptions options = new ChromeOptions();
+        //    if (driverOptions == "headless")
+        //    {
+
+        //        options.AddArgument("--headless");
+        //    }
+        //    driver = new ChromeDriver(startupPath, options);
+        //}
+    }
+}
